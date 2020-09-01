1256619e11e6edc56ff2a94755e24335e5404497
dercraft <wuyuhang2073@gmail.com>
2020-09-01 21:24:29 +0800

    temporal test for gdas model

--- test/gdas_test.py
+++ test/gdas_test.py
@@ -1,7 +1,10 @@
+import os
+import sys
 import numpy as np
 import torch
 import torch.nn.functional as F
 
+sys.path.append('.')
 from xnas.core.timer import Timer
 from xnas.search_space.cell_based import GDAS
 
@@ -12,9 +15,9 @@ def basic_darts_cnn_test():
     search_net = GDAS().cuda()
     _random_architecture_weight = torch.randn(
         [search_net.num_edges * 2, len(search_net.basic_op_list)]).cuda()
-    _input = torch.randn([2, 3, 32, 32]).cuda()
+    _input = torch.randn([4, 3, 32, 32]).cuda()
     time_.tic()
-    _out_put = search_net(_input, _random_architecture_weight)
+    _out_put = search_net.forwardGDAS(_input, _random_architecture_weight)
     time_.toc()
     print(_out_put.shape)
     print(time_.average_time)
@@ -23,7 +26,7 @@ def basic_darts_cnn_test():
                                    np.random.choice(len(search_net.basic_op_list), search_net.num_edges * 2)]).cuda()
     _input = torch.randn([2, 3, 32, 32]).cuda()
     time_.tic()
-    _out_put = search_net(_input, _random_one_hot)
+    _out_put = search_net.forwardGDAS(_input, _random_one_hot)
     time_.toc()
     print(_out_put.shape)
     print(time_.average_time)
